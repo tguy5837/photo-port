3b724e09a3a7782bb30b89de2e3c2425ae6893c6
DTGuy <tguy5837@gmail.com>
2022-12-08 19:44:54 -0500

    added form functionality

---
+++ src/components/Contact/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState)
+    }
+
+    // JSX
+    return (
+        <section>
+            <h1>Contact me</h1>
+            <form id='contact-form' onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor='name'>Name:</label>
+                    <input type='text' name='name' defaultValue={name} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor='email'>Email:</label>
+                    <input type='text' name='email' defaultValue={email} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor='message'>Message:</label>
+                    <textarea name='message' rows='5' defaultValue={message} onChange={handleChange} />
+                </div>
+                <button type='submit'>Submit</button>
+            </form>
+        </section>
+    )
+}
+
+export default ContactForm;

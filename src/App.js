5fcb2afd0d0695413552ff905fc9fa2522bec266
DTGuy <tguy5837@gmail.com>
2022-12-08 21:10:06 -0500

    added form validation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
+
+
   const [categories] = useState([
     {
       name: 'commercial',
@@ -23,11 +26,17 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
